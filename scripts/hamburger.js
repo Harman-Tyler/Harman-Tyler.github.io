e2bde2497b9ac2055bbb1df961bccd3b58ca5587
Harman-Tyler <38660307+Harman-Tyler@users.noreply.github.com>
2018-07-27 18:15:31 -0600

    trying to change things back
    to how they were

--- scripts/hamburger.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
-    
-    if (document.getElementById("HambugerButton").innerHTML == "☰") {
-        document.getElementById("HambugerButton").innerHTML = "❎";
-    }else{
-        document.getElementById("HambugerButton").innerHTML = "☰";
-    }
-}
